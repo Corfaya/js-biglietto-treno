a7909c5da1185c71c213105c53e9b937257c58e5
Corfaya <lauracorrado0696@gmail.com>
2024-06-25 15:48:31 +0200

    [UPD] km and age variables declared and initialized. Console.log() debug added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-alert("hello")
+// alert("hello")
+
+// VARIABLES
+let km = prompt("Inserire qui il numero di km che si intende percorrere")
+console.log(km)
+let age = prompt("Inserire qui l'età del passeggero")
+console.log(age)
