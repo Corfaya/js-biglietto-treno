9c8b534c6f2c7b9b2484af142d10bd4c63e57b3f
Corfaya <lauracorrado0696@gmail.com>
2024-06-25 15:51:37 +0200

    [UPD] ticket price for km, ticket price with specific number of km and reduction's variables added. Debug test with console.log()

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 // alert("hello")
 
 // VARIABLES
-let km = prompt("Inserire qui il numero di km che si intende percorrere")
+let km = prompt("Inserire qui il numero di km che si intende percorrere");
 console.log(km)
-let age = prompt("Inserire qui l'età del passeggero")
+let age = prompt("Inserire qui l'età del passeggero");
 console.log(age)
+let costPerKm = 0.21;
+console.log(costPerKm)
+let price = costPerKm * km;
+console.log(price);
+let reduction = 0;
